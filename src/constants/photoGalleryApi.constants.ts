7d9a427d11dba1bf9cdd952e69dbf8ed6076f462
Amila <samaranayake@axinom.com>
2021-05-12 00:23:44 +0530

    Server port changed

--- src/constants/photoGalleryApi.constants.ts
+++ src/constants/photoGalleryApi.constants.ts
@@ -9,7 +9,7 @@ const {
     MONGO_DB
 } = process.env; 
 
-export const PORT = SERVER_PORT || 9010;
+export const PORT = SERVER_PORT || 3010;
 export const APPLICATION_TOKEN = APP_TOKEN || "YvlVLbj7mBm2WIHyQGO53A==";
 export const APPLICATION_VERSION = APP_VERSION || "0.3.0";
 export const MONGO_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
